08e68e7c424595a100966e180f4579190545ad39
bangyulok <cafebanyuloka@gmail.com>
2025-05-30 04:05:17 +0700

    update server

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ onGift((gift) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`✅ Server berjalan di http://localhost:${PORT}`);
 });
